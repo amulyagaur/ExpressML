daa79efca76765c6555204f7d9a7e2e1a2be6762
lazy-pro <shivamanand98@gmail.com>
2019-01-26 02:18:41 +0530

    add classification models script

---
+++ script_KNN.py
@@ -0,0 +1,64 @@
+import sys, json
+import pandas as pd
+import numpy as np
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from time import time 
+
+df = pd.read_csv("data.csv") 	
+
+train,test = train_test_split(df,test_size=0.9)
+
+lines = sys.stdin.readlines()
+
+trainfeatures = lines[0][1:-2]
+label =lines[1][:-1]
+
+features='\",'+trainfeatures+',\"'
+feat = features.split('","')
+feat = feat[1:-1]
+desiredno = len(feat)
+
+features_test = test[feat]
+
+labels_train = train[label]
+
+features_train = train[feat]
+
+labels_test = test[label]
+
+#feature selection
+select = SelectKBest(chi2, k=3).fit(features_train, labels_train)
+ranking = select
+cols = select.get_support(indices=True)
+rank = select.scores_
+mask = select.get_support()
+new_features = features_train.columns[mask]
+features_train = features_train[new_features]
+features_test = features_test[new_features]
+
+# print "KNN :"
+t0 =time()
+grid_param = {'n_neighbors':[1,2,3],
+          'leaf_size':[2,5],
+          'weights':['uniform', 'distance'],
+          'algorithm':['auto', 'ball_tree','kd_tree','brute'],
+          'n_jobs':[-1]}
+from sklearn.neighbors import KNeighborsClassifier
+clf1 = KNeighborsClassifier(n_neighbors=3,algorithm='ball_tree')
+gd_sr = GridSearchCV(estimator=clf1,  
+                     param_grid=grid_param,
+                     scoring='accuracy',
+                     cv=2,
+                     n_jobs=-1)
+gd_sr.fit(features_train,labels_train)
+
+modelPred = 100*gd_sr.best_score_
+modeltune = gd_sr.best_params_
+trainTime = round(time()-t0,3)
+
+print(modelPred)
+print(modeltune)
+print(trainTime)
---
+++ script_NaiveBayes.py
@@ -0,0 +1,50 @@
+import sys, json
+import pandas as pd
+import numpy as np
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from time import time 
+
+df = pd.read_csv("data.csv") 	
+
+train,test = train_test_split(df,test_size=0.9)
+
+lines = sys.stdin.readlines()
+
+trainfeatures = lines[0][1:-2]
+label =lines[1][:-1]
+
+features='\",'+trainfeatures+',\"'
+feat = features.split('","')
+feat = feat[1:-1]
+desiredno = len(feat)
+
+features_test = test[feat]
+
+labels_train = train[label]
+
+features_train = train[feat]
+
+labels_test = test[label]
+
+#feature selection
+select = SelectKBest(chi2, k=3).fit(features_train, labels_train)
+ranking = select
+cols = select.get_support(indices=True)
+rank = select.scores_
+mask = select.get_support()
+new_features = features_train.columns[mask]
+features_train = features_train[new_features]
+features_test = features_test[new_features]
+
+# Gaussian naive_bayes :
+t0 = time()
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+modelPred = 100*clf.score(features_test,labels_test)
+trainTime = (time()-t0)
+print(modelPred)
+print(trainTime) 
